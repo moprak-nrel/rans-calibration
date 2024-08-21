949f6772c4b6cb9bf410f5f06186476033ab5d1e
prakash <prakash.mohan@nrel.gov>
2024-08-21 10:09:43 -0500

    Use multprocessing to speed up computations

--- emcee_calibration.py
+++ emcee_calibration.py
@@ -1,3 +1,7 @@
+import os
+import time
+from multiprocessing import Pool
+
 import corner
 import emcee
 import matplotlib.pyplot as plt
@@ -5,6 +9,8 @@ import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 
 import run_rans_nofcs as rr
+
+os.environ["OMP_NUM_THREADS"] = "1"
 
 plt.style.use("project.mplstyle")
 np.random.seed(42)
@@ -67,41 +73,50 @@ class Posterior:
         return likelihood(theta, self.data, self.sigma) + prior(theta)
 
 
-# Set true parameters and generate data
-f = 0.5
-theta_true = np.array([0.02, 1.0, 2.0])
-print("True values: ", theta_true)
-sigma_meas = 0.01
-data = gen_data(theta_true, sigma_meas)
+if __name__ == "__main__":
+    # Set true parameters and generate data
+    f = 0.5
+    theta_true = np.array([0.02, 1.0, 2.0])
+    print("True values: ", theta_true)
+    sigma_meas = 0.01
+    data = gen_data(theta_true, sigma_meas)
 
+    # Proxy likelihood function for MLE using scipy
+    def cost_function(*args):
+        return -likelihood(*args)
 
-# Proxy likelihood function for MLE using scipy
-def cost_function(*args):
-    return -likelihood(*args)
+    theta0 = [5.48 ** (-2), 1.176, 1.92]
+    ## Compute the MLE estimate
+    ## This is currently commented out because it doesn't converge
+    # MLE = sp.optimize.minimize(cost_function, theta0, args=(data, sigma_meas))
+    # theta_mle = MLE.x
+    # print("MLE : ", theta_mle)
 
+    # Setup the posterior object
+    post = Posterior(data, sigma_meas)
 
-theta0 = [5.48 ** (-2), 1.176, 1.92]
-# MLE = sp.optimize.minimize(cost_function, theta0, args=(data, sigma_meas))
-# theta_mle = MLE.x
-# print("MLE : ", theta_mle)
+    # Initialize the number of chains
+    n_walkers = 6
+    n_dim = len(theta_true)
+    theta0 = theta0 + 0.01 * np.random.rand(n_walkers, n_dim)
 
-# Setup the posterior object
-post = Posterior(data, sigma_meas)
+    # Run the MCMC to get posterior samples
+    with Pool() as pool:
+        sampler = emcee.EnsembleSampler(n_walkers, n_dim, post, pool=pool)
+        start_time = time.time()
+        sampler.run_mcmc(theta0, 100, progress=True)
+        end_time = time.time()
+    print(f"Total time for calibration: {end_time - start_time}")
 
-# Initialize the number of chains
-n_walkers = 6
-n_dim = len(theta_true)
-theta0 = theta0 + 0.01 * np.random.rand(n_walkers, n_dim)
-# Run the MCMC to get posterior samples
-sampler = emcee.EnsembleSampler(n_walkers, n_dim, post)
-sampler.run_mcmc(theta0, 1000, progress=True)
-# Flatten the chains, and thin them out
-flat_samples = sampler.get_chain(discard=0, thin=10, flat=True)
+    # Flatten the chains, and thin them out
+    flat_samples = sampler.get_chain(discard=0, thin=10, flat=True)
 
-fig_name = "posterior.pdf"
-with PdfPages(fig_name) as pdf:
-    # Plot the posterior
-    fig = corner.corner(
-        flat_samples, labels=[r"$C_{\mu}$", r"$C_{1e}$", r"$C_{2e}$"], truths=theta_true
-    )
-    pdf.savefig(figure=fig)
+    fig_name = "posterior.pdf"
+    with PdfPages(fig_name) as pdf:
+        # Plot the posterior
+        fig = corner.corner(
+            flat_samples,
+            labels=[r"$C_{\mu}$", r"$C_{1e}$", r"$C_{2e}$"],
+            truths=theta_true,
+        )
+        pdf.savefig(figure=fig)
