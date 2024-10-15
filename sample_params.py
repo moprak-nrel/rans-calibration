df217d6f8ea0786719497773fa64de0bbfc68b07
prakash <prakash.mohan@nrel.gov>
2024-10-15 14:26:10 -0600

    update sampler to do a grid search on the 3 params along with individual variations

--- sample_params.py
+++ sample_params.py
@@ -53,7 +53,9 @@ kinit = k_smooth.copy()
 cmu = 5.48 ** (-2)
 c1 = 1.176
 c2 = 1.92
-n_samples = 2
+n_samples = 5
+
+## Run RANS with individual variations of the 3 coefficients
 cmus = np.linspace(0.5 * cmu, 1.5 * cmu, n_samples)
 c1s = np.linspace(0.5 * c1, 1.5 * c1, n_samples)
 c2s = np.linspace(0.5 * c2, 1.5 * c2, n_samples)
@@ -62,23 +64,10 @@ params_c1 = [[cmu, c1_, c2] for c1_ in c1s]
 params_c2 = [[cmu, c1, c2_] for c2_ in c2s]
 
 
-def run_rans_samples(Uinit, kinit, param_array, rvec):
-    qois = []
-    for i, params in enumerate(param_array):
-        try:
-            print(f"Running RANS with parameters: {params}")
-            res = utils.run_rans(Uinit, kinit, params, rvec)
-            qois.append(res["wake_stats"])
-        except Exception:
-            print(f"Failed for parameters: {params}")
-            qois.append(None)
-    return qois
-
-
 qoi_samples = {}
-qoi_samples["cmu"] = run_rans_samples(Uinit, kinit, params_cmu, rvec)
-qoi_samples["c1"] = run_rans_samples(Uinit, kinit, params_c1, rvec)
-qoi_samples["c2"] = run_rans_samples(Uinit, kinit, params_c2, rvec)
+qoi_samples["cmu"] = utils.run_rans_samples(Uinit, kinit, params_cmu, rvec)
+qoi_samples["c1"] = utils.run_rans_samples(Uinit, kinit, params_c1, rvec)
+qoi_samples["c2"] = utils.run_rans_samples(Uinit, kinit, params_c2, rvec)
 
 labels = {}
 labels["cmu"] = [r"$C_\mu$" + f" = {cmu_:.3f}" for cmu_ in cmus]
@@ -86,61 +75,17 @@ labels["c1"] = [r"$C_1$" + f" = {c1_:.3f}" for c1_ in c1s]
 labels["c2"] = [r"$C_2$" + f" = {c2_:.3f}" for c2_ in c2s]
 
 
-def plot_qoi_variation(qois, labels, fig_name):
-    color_list = plt.cm.viridis(np.linspace(0, 1, len(qois)))
-    with PdfPages(fig_name) as pdf:
-        plt.figure()
-        for i, qoi in enumerate(qois):
-            c = color_list[i]
-            wake_stats = qoi
-            if wake_stats is None:
-                continue
-            plt.plot(
-                wake_stats[:, 0] / diameter,
-                wake_stats[:, 1] * 2 * np.pi,
-                label=labels[i],
-                c=c,
-                ls="solid",
-            )
-        plt.plot(
-            (wake_stats_les[:, 0] - 1200) / diameter,
-            wake_stats_les[:, 1],
-            label="LES",
-            color="k",
-            alpha=0.5,
-            ls="dotted",
-        )
-        plt.legend()
-        plt.xlabel("x/D")
-        plt.ylabel(r"Displacement Thickness ($m^2$)")
-        pdf.savefig()
-
-        plt.figure()
-        for i, qoi in enumerate(qois):
-            c = color_list[i]
-            wake_stats = qoi
-            if wake_stats is None:
-                continue
-            plt.plot(
-                wake_stats[:, 0] / diameter,
-                wake_stats[:, 2] * 2 * np.pi,
-                label=labels[i],
-                c=c,
-                ls="solid",
-            )
-        plt.plot(
-            (wake_stats_les[:, 0] - 1200) / diameter,
-            wake_stats_les[:, 2],
-            label="LES",
-            color="k",
-            alpha=0.5,
-            ls="dotted",
-        )
-        plt.legend()
-        plt.xlabel("x/D")
-        plt.ylabel(r"Momentum Thickness ($m^2$)")
-        pdf.savefig()
-
-
 for p in qoi_samples.keys():
-    plot_qoi_variation(qoi_samples[p], labels[p], f"{p}_samples.pdf")
+    utils.plot_qoi_variation(
+        qoi_samples[p], labels[p], f"{p}_samples.pdf", wake_stats_les
+    )
+
+## Run RANS simultaneously varying all the coefficients
+params_list = [[cmu_, c1_, c2_] for cmu_ in cmus for c1_ in c1s for c2_ in c2s]
+labels = [
+    f"Cmu = {cmu_:.3f}, c1 = {c1_:.3f}, c2 = {c2_:.3f}"
+    for cmu_, c1_, c2_ in params_list
+]
+
+qois = utils.run_rans_samples(Uinit, kinit, params_list, rvec)
+utils.plot_qoi_variation(qois, labels, "params_samples.pdf", wake_stats_les)
